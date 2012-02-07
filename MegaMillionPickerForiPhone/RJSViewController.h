1de9a2980f2578623daa329276c2fbfbc60b7c50
Reed Stoner <kaltekar@gmail.com>
2012-02-07 00:08:27 -0500

    Added the start of some UI Wiz

--- MegaMillionPickerForiPhone/RJSViewController.h
+++ MegaMillionPickerForiPhone/RJSViewController.h
@@ -12,7 +12,8 @@
 
 @interface RJSViewController : UIViewController
 
-@property (nonatomic, strong) SCLotteryEngine *lottoEngine;;
+@property (nonatomic, strong) SCLotteryEngine *lottoEngine;
+@property BOOL isStartup;
 
 @property (weak, nonatomic) IBOutlet UILabel *ball1Label;
 @property (weak, nonatomic) IBOutlet UILabel *ball2Label;
@@ -20,6 +21,15 @@
 @property (weak, nonatomic) IBOutlet UILabel *ball4Label;
 @property (weak, nonatomic) IBOutlet UILabel *ball5Label;
 @property (weak, nonatomic) IBOutlet UILabel *megaballLabel;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *directionLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *ball1ImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *ball2ImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *ball3ImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *ball4ImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *ball5ImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *megaballImageView;
+
 
 
 
--- MegaMillionPickerForiPhone/RJSViewController.m
+++ MegaMillionPickerForiPhone/RJSViewController.m
@@ -17,12 +17,38 @@
 @synthesize ball4Label;
 @synthesize ball5Label;
 @synthesize megaballLabel;
-@synthesize lottoEngine = _lottoEngine;
+@synthesize titleLabel;
+@synthesize directionLabel;
+@synthesize ball1ImageView;
+@synthesize ball2ImageView;
+@synthesize ball3ImageView;
+@synthesize ball4ImageView;
+@synthesize ball5ImageView;
+@synthesize megaballImageView;
 
+@synthesize lottoEngine = _lottoEngine;
+@synthesize isStartup = _isStartup;
 
 - (void) getPicks:(UITapGestureRecognizer *)recognizer {
     
     NSArray *array = [self.lottoEngine picks];
+    
+    if (self.isStartup) {
+        self.titleLabel.hidden = NO;
+        self.ball1Label.hidden = NO;
+        self.ball2Label.hidden = NO;
+        self.ball3Label.hidden = NO;
+        self.ball4Label.hidden = NO;
+        self.ball5Label.hidden = NO;
+        self.megaballLabel.hidden = NO;
+        self.directionLabel.hidden = YES;
+        self.ball1ImageView.hidden = NO;
+        self.ball2ImageView.hidden = NO;
+        self.ball3ImageView.hidden = NO;
+        self.ball4ImageView.hidden = NO;
+        self.ball5ImageView.hidden = NO;
+        self.megaballImageView.hidden = NO;
+    }
     
     self.ball1Label.text = [NSString stringWithFormat:@"%@",[array objectAtIndex:0]];
     self.ball2Label.text = [NSString stringWithFormat:@"%@",[array objectAtIndex:1]];
@@ -39,15 +65,55 @@
     // Release any cached data, images, etc that aren't in use.
 }
 
+- (BOOL)canBecomeFirstResponder {
+    return YES;
+}
+
+- (void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event {
+    
+}
+
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
+
+    [self getPicks:nil];
+}
+
+- (void)motionCancelled:(UIEventSubtype)motion withEvent:(UIEvent *)event {
+    
+}
+
+
 #pragma mark - View lifecycle
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	_lottoEngine = [[SCLotteryEngine alloc] init];
+	
+    _lottoEngine = [[SCLotteryEngine alloc] init];
+    
+    
     
     UITapGestureRecognizer *gr = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(getPicks:)];
     [self.view addGestureRecognizer:gr];
+    
+
+    self.titleLabel.hidden = YES;
+    self.ball1Label.hidden = YES;
+    self.ball2Label.hidden = YES;
+    self.ball3Label.hidden = YES;
+    self.ball4Label.hidden = YES;
+    self.ball5Label.hidden = YES;
+    self.megaballLabel.hidden = YES;
+    self.directionLabel.hidden = NO;
+    self.ball1ImageView.hidden = YES;
+    self.ball2ImageView.hidden = YES;
+    self.ball3ImageView.hidden = YES;
+    self.ball4ImageView.hidden = YES;
+    self.ball5ImageView.hidden = YES;
+    self.megaballImageView.hidden = YES;
+
+    self.isStartup = YES; 
+    
     
 }
 
@@ -59,6 +125,14 @@
     [self setBall4Label:nil];
     [self setBall5Label:nil];
     [self setMegaballLabel:nil];
+    [self setTitleLabel:nil];
+    [self setBall1ImageView:nil];
+    [self setBall2ImageView:nil];
+    [self setBall2ImageView:nil];
+    [self setBall4ImageView:nil];
+    [self setBall5ImageView:nil];
+    [self setMegaballImageView:nil];
+    [self setDirectionLabel:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -67,6 +141,7 @@
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
+    [self becomeFirstResponder];
 }
 
 - (void)viewDidAppear:(BOOL)animated
